04226dc91d9cc5318322e1ad63b39aa112c1e9e6
Sugihara <s-koki.sugihara@rakuten.com>
2020-10-15 18:56:57 +0900

    a

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,28 +1,92 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
+
 class ProfileController extends Controller
 {
-    public function add()
-    {
-        return view('admin.profile.create');
-    }
-    
-    public function create(Request $request)
-    {
-        return redirect('admin/profile/create');
-    }
-    
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
-    
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-}
+  public function add()
+  {
+      return view('admin.profile.create');
+  }
+
+  public function create(Request $request)
+  {
+
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+
+      // formに画像があれば、保存する
+      if ($form['image']) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+        $profile->image_path = null;
+      }
+
+      unset($form['_token']);
+      unset($form['image']);
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+  }
+
+  public function index(Request $request)
+  {
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          $posts = Profile::all();
+      }
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+
+  public function edit(Request $request)
+  {
+      
+    $profile = Profile::find($request->id);
+
+      return view('admin.news.edit', ['news_form' => $profile]);
+  }
+
+
+  public function update(Request $request)
+  {
+      // Validationをかける
+      $this->validate($request, Profile::$rules);
+      // News Modelからデータを取得する
+      $profile = Profile::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $profile_form = $request->all();
+      unset($profile_form['_token']);
+
+      // 該当するデータを上書きして保存する
+      $profile->fill($profile_form)->save();
+
+      return redirect('admin/profile/');
+  }
+
+  // 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $profile = Profile::find($request->id);
+      // 削除する
+      $profile->delete();
+      return redirect('admin/profile/');
+  }  
+
+
 }
